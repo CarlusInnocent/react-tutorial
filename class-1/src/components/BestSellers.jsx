a5838e405298dbedd0f393491f66f51a2263a883
CarlusInnocent <carlusinnocentainembabazi@gmail.com>
2024-10-29 00:28:53 +0300

    props finalizing

--- class-1/src/components/BestSellers.jsx
+++ class-1/src/components/BestSellers.jsx
@@ -1,16 +1,51 @@
 import Book from "./Book";
 import styles from "./../styles/Global.module.css";
 
+const book1 = {
+    image: "/../../public/images/pic1.jpg",
+    title: "Patriot",
+    writter: "J. K. Rowling",
+}
+
+const book2 = {
+    image: "/../../public/images/4.jpg",
+    title: "Patriot",
+    writter: "J. K. Rowling",
+}
+
+const book3 = {
+    image: "/../../public/images/pic3.jpg",
+    title: "Patriot",
+    writter: "J. K. Rowling",
+}
+
 export default function BestSellers() {
     return (
         <>
         <h1>These are the best sellers</h1>
         <div className={styles["best-sellers"]}>
             
-            <Book />
-            <Book />
-            <Book />
+            <Book 
+            image={book1.image}
+            title={book1.title}
+            writter={book1.writter}
+            >
+                <h1>Hello, World</h1>
+                <p>THis is inside the first book component</p>
+            </Book>
+            <Book 
+            image={book2.image}
+            title={book2.title}
+            writter={book2.writter}
+            />
+            <Book 
+            image={book3.image}
+            title={book3.title}
+            writter={book3.writter}
+            />
         </div>
         </>
     );
 }
+
+
--- class-1/src/components/Book.jsx
+++ class-1/src/components/Book.jsx
@@ -1,13 +1,95 @@
+import { Children } from "react";
 import styles from "./../styles/Global.module.css";
+import PropTypes from "prop-types";
 
-export default function Book() {
+
+//using props object
+export default function Book(props) {
+    console.log(props);
     return(
         <>
         <div className={styles["seller-card"]}>
-            <img src="./public/images/pic1.jpg" alt="patriot" />
-            <p className={styles["title"]}>Patriot</p>
-            <p className={styles["writter"]}>Alexei Navalny</p>
+            <img src={props.image} />
+            <p className={styles["title"]}>{props.title}</p>
+            <p className={styles["writter"]}>{props.writter}</p>
+            <div className="content">{props.children}</div>
         </div>
         </>
     );
 }
+
+Book.propTypes = {
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string,
+    writter: PropTypes.string,
+    children: PropTypes.string
+}
+
+
+//destructuring the props
+const book1 = ({image, title, writter}) => {
+    return(
+        <>
+        <div className={styles["seller-card"]}>
+            <img src={image} />
+            <p className={styles["title"]}>{title}</p>
+            <p className={styles["writter"]}>{writter}</p>
+        </div>
+        </>
+    );
+};
+
+//default props
+book1.defaultProps = {
+    image: "/../../public/images/pic1.jpg",
+    title: "Patriot",
+    writter: "J. K. Rowling"
+}
+
+
+//children prop
+//Done
+
+//simple list
+// Usage
+//< book2 books={['AnimalFarm', 'HurryPoter', 'JaneEyre']} />
+//
+// const book2 = (props) => {
+//     return(
+//         <>
+//             <ul>
+//                 {props.books.map(
+//                     (book, index) => (
+//                         <li key={index}>{book}</li>
+//                     )
+//                 )}
+//             </ul>
+//         </>
+//     );
+// };
+//AnimalFarm
+//HurryPoter
+//JaneEyre
+
+// usage 
+/*
+const movieData = [
+    { id: 1, title: 'The Shawshank Redemption', year: 1994 },
+    { id: 2, title: 'The Godfather', year: 1972 },
+    { id: 3, title: 'The Godfather: Part II', year: 1974 },
+];
+
+<book3 movies={movieData} />
+*/
+
+// const MoviesList = ({movies}) => {
+//     return(
+//         <>
+//             <ul>
+//                 {movies.map((movie) => (
+//                     <li key={movie.id}>{movie.title}</li>
+//                 ))}
+//             </ul>
+//         </>
+//     );
+// };
